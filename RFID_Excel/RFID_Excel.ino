bae82845a806c82f3b98df4455a9ec28d48cb57d
Srianant1 <101706189+Srianant1@users.noreply.github.com>
2023-03-08 22:15:29 +0530

    Add files via upload
    Some excel code I found. 
    I made changes so it shows the labels.

---
+++ RFID_Excel/RFID_Excel.ino
@@ -0,0 +1,97 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10 //RX slave select
+#define RST_PIN 9
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
+byte card_ID[4]; 
+byte Name1[4]={0xAA,0x48,0xF7,0xAF};//first UID card
+byte Name2[4]={0x63,0x3A,0xB1,0x98};//second UID card
+
+
+int NumbCard[2];
+int j=0;        
+
+int const RedLed=6;
+int const GreenLed=5;
+int const Buzzer=8;
+
+String Name;//user name
+long Price;//user number
+int n ;//The number of card you want to detect (optional)  
+
+void setup() {
+  Serial.begin(9600); 
+  SPI.begin();  // SPI bus
+  mfrc522.PCD_Init(); // MFRC522 card
+  
+  Serial.println("CLEARSHEET");                 
+  Serial.println("LABEL,Brand,Type,Name,Price");
+
+  pinMode(RedLed,OUTPUT);
+  pinMode(GreenLed,OUTPUT);
+  pinMode(Buzzer,OUTPUT);
+
+   }
+    
+void loop() {
+  //look for new card
+   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  return;//got to start of loop if there is no card present
+ }
+ // Select one of the cards
+ if ( ! mfrc522.PICC_ReadCardSerial()) {
+  return;//if read card serial(0) returns 1, the uid struct contians the ID of the read card.
+ }
+ 
+ for (byte i = 0; i < mfrc522.uid.size; i++) {
+     card_ID[i]=mfrc522.uid.uidByte[i];
+
+       if(card_ID[i]==Name1[i]){
+       Name="Nike Running Shes";//user name
+       Price=4562;//user number
+       j=0;//first number in the NumbCard array : NumbCard[j]
+      }
+      else if(card_ID[i]==Name2[i]){
+       Name="Mens Solid Track Pant";//user name
+       Price=1871;//user number
+       j=1;//Second number in the NumbCard array : NumbCard[j]
+      }
+      else{
+          digitalWrite(GreenLed,LOW);
+          digitalWrite(RedLed,HIGH);
+          goto cont;//go directly to line 85
+     }
+}
+      if(NumbCard[j] == 1){//to check if the card already detect
+      //if you want to use LCD
+      //Serial.println("Already Exist");
+      }
+      else{
+      NumbCard[j] = 1;//put 1 in the NumbCard array : NumbCard[j]={1,1} to let the arduino know if the card was detecting 
+      n++;//(optional)
+      Serial.print("DATA,Brand,Type," + Name);//send the Name to excel
+      Serial.print(",");
+      Serial.println(Price); //send the Number to excel
+      digitalWrite(GreenLed,HIGH);
+      digitalWrite(RedLed,LOW);
+      digitalWrite(Buzzer,HIGH);
+      delay(30);
+      digitalWrite(Buzzer,LOW);
+      Serial.println("SAVEWORKBOOKAS,Names/FabricNames");
+      }
+      delay(1000);
+cont:
+delay(2000);
+digitalWrite(GreenLed,LOW);
+digitalWrite(RedLed,LOW);
+
+//if you want to close the Excel when all card had detected and save Excel file in Names Folder. in my case i have just 2 card (optional)
+//if(n==2){
+    
+  //  Serial.println("FORCEEXCELQUIT");
+ //   }
+}
+    
